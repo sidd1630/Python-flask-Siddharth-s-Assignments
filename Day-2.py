10c231adff2fcd893e0e41824b6f22e0cdec8093
sidd1630 <83753030+sidd1630@users.noreply.github.com>
2021-05-06 13:21:11 +0530

    Add files via upload

---
+++ Day-2.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+"""Conversion from int to str, float, bool"""
+
+
+#%%
+value = 5
+print(type(value),value)
+value = str(value)
+print(type(value),value)
+#%%
+value = 5
+print(type(value),value)
+value = float(value)
+print(type(value),value)
+#%%
+value = 5
+print(type(value),value)
+value = bool(value)
+print(type(value),value)
+
+#%%
+"""Conversion from str to int, float, bool"""
+
+value = '5'
+print(type(value),value)
+value = int(value)
+print(type(value),value)
+
+#%%
+value = '5'
+print(type(value),value)
+value = float(value)
+print(type(value),value)
+#%%
+value = '5'
+print(type(value),value)
+value = bool(value)
+print(type(value),value)
+
+#%%
+"""Conversion from float to int, str, bool"""
+
+value = 5.0
+print(type(value),value)
+value = int(value)
+print(type(value),value)
+#%%
+value = 5.0
+print(type(value),value)
+value = str(value)
+print(type(value),value)
+
+#%%
+value = 5.0
+print(type(value),value)
+value = bool(value)
+print(type(value),value)
+
+#%%
+"""Conversion from bool to int, str, float"""
+
+value = True
+print(type(value),value)
+value = int(value)
+print(type(value),value)
+
+#%%
+value = True
+print(type(value),value)
+value = str(value)
+print(type(value),value)
+
+#%%
+value = True
+print(type(value),value)
+value = float(value)
+print(type(value),value)
+
